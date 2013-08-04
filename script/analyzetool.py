6ea9ded5c1c114a31fbfb5f4dff75e6ba7c9c2f6
老土 <xingsun.tu@gmail.com>
2013-08-04 09:48:52 +0800

    V2.2 多个日志文件使用异步线程并行运行
    400M的文件，运行需要26s，对比三个，macth，split，keyfactor，时间花费在split及keyfactor上，这两个方面均有
    优化的空间。

--- script/analyzetool.py
+++ script/analyzetool.py
@@ -3,42 +3,55 @@
 
 import re
 import paramiko
+import time
 
 import keyfactor
 import errorinfo
-import loglist
 
-def analyze():
-    serverlist = loglist.getloglist()
-    for s in serverlist:
-        host = s.host
-        path = s.path
-        filename = s.filename
-        username = s.user
-        password = s.password
-        print host + username + password
-        analyzeSignal(host, path, filename, username, password)
-
-def analyzeSignal(host, path, filename, username, password):
-    port = 22
+def analyze(host, path, filename, username, password, port):
+    
+    print 'start to analyze ' + host + '-' + filename + '\n'
+    start = time.time()
+    
     transport = paramiko.Transport((host, port))
     transport.connect(username=username, password=password)
     sftp = paramiko.SFTPClient.from_transport(transport)
     sftp.chdir(path)
     fileHandle = sftp.open(filename, 'r')
+    dowords(fileHandle, host, filename)
+    fileHandle.close()
+    transport.close()
+    
+    end = time.time();
+    print 'finished to analyze' + host + '-' + filename
+    print ',total time is %f \n' % (end - start)
+    
+def getheadwords(line):
+    return re.split(r'\s{3}', line);
+    
+def getheadwords2(line):
+    return re.split(r'\s+', line)
+
+def dowords(fileHandle, host, filename):
     keylist = []
     switch = False
     content = ''
     for line in fileHandle:
         timefmt = '[0-9]{4}-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]'
+        # start = time.time()
         head = re.match(timefmt, line)
+        # end = time.time()
+        # print 'math spends %f \n' % (end - start) 
         if head:
-            allwords = re.split(r'\s+', line)
+            # start = time.time()
+            allwords = getheadwords2(line)
             key = allwords[3]
-            
+            # end = time.time()
+            # print 'split spends %f \n' % (end - start)
             it = iter(keylist)
             exist = False
-    
+            
+            # start = time.time()
             switch = False
             try:
                 while True: 
@@ -50,7 +63,7 @@ def analyzeSignal(host, path, filename, username, password):
                 pass
             if exist == False:
                 factor = keyfactor.KEYFACTOR()
-                factor.key_word = key
+                factor.key_word = allwords[3]
                 factor.key_content = line
                 factor.key_count = 1
                 factor.first_time = allwords[1]
@@ -67,7 +80,6 @@ def analyzeSignal(host, path, filename, username, password):
         else:
             if switch == True:
                 content = content + line
-
-    fileHandle.close()
-    transport.close()
+        # end = time.time()
+        # print 'keyfactor spends %f \n' % (end - start)
     errorinfo.recordError(host, keylist, content)
--- script/watchdog.py
+++ script/watchdog.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 # _*_ coding:utf-8 _*_
 
+import threading
 import analyzetool
-analyzetool.analyze()
+import loglist
+
+# 批量分析日志
+def analyze_batch():
+    serverlist = loglist.getloglist()
+    port = 22
+    for s in serverlist:
+        th = threading.Thread(target=analyzetool.analyze, args=(s.host, s.path, s.filename, s.user, s.password, port))
+        th.start()
+        
+analyze_batch()
